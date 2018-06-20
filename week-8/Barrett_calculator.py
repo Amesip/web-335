8031951b26d21cb70210fa3bbebed6144ca102d7
Mary Barrett <mkbarrett.1201@gmail.com>
2018-06-20 18:42:58 -0500

    Adding Week 8

---
+++ week-8/Barrett_calculator.py
@@ -0,0 +1,13 @@
+def add(var1, var2):
+    return "The total is {} ".format(var1 + var2)
+
+def subtract(var1, var2):
+    return "The total is {} ".format(var1 - var2)
+
+
+def divide(var1, var2):
+    return "The total is {} ".format(var1 / var2)
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8, 2))
